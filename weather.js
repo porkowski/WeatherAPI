c0bf768948a41fc12d554bfae1c62b641175db29
porkowski <borkowskipatryk10@gmail.com>
2024-02-11 11:01:54 -0500

    need to transfer objects into visible weather blocks using divs in css

--- weather.js
+++ weather.js
@@ -1,11 +1,10 @@
-const { max } = require("date-fns");
-
 //Grab form elements from DOM
 const form = document.getElementById("formId");
 const text = document.getElementById("Location");
 
 //Create constructor for Forecast
-function Forecast(sunrise,sunset,condition,conditionCode,maxtempC,mintempC,maxtempF,mintempF,totalprecipIn,totalprecipMm,tsnowCm,maxwindKph,maxwindMph) {
+function Forecast(date,sunrise,sunset,condition,conditionCode,maxtempC,mintempC,maxtempF,mintempF,totalprecipIn,totalprecipMm,tsnowCm,maxwindKph,maxwindMph) {
+    this.date=date;
     this.sunrise=sunrise;
     this.sunset=sunset;
     this.condition=condition;
@@ -21,6 +20,7 @@ function Forecast(sunrise,sunset,condition,conditionCode,maxtempC,mintempC,maxte
     this.maxwindMph=maxwindMph;
 }
 
+
 async function getForecast(loc) {
     try {
     const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=3b6092868dac4a54889194837232611&q=${loc}&days=3`, {
@@ -29,7 +29,7 @@ async function getForecast(loc) {
     const json = await response.json();
     const array = json.forecast.forecastday;
     //Return array holding 3 days worth of weather for location
-    return array[0];
+    return array;
     } catch(error) {
         console.log(error);
     }
@@ -40,8 +40,49 @@ function submitForm(event) {
     //Prevent form from submitting, just grab location and call getForecast();
     event.preventDefault();
     const location = text.value;
-    getForecast(location).then(response=>console.log(response));
+    getForecast(location).then(response=>createObjects(response)).then(info=>console.log(info[0])).catch(error=>alert(error));
 }
+
+function getData(array) {
+    let forecastarray= [];
+    for (date of array) {
+        //Create variables for each parameter that will be used to create new Forecast Object
+        const astro = date.astro;
+        const d = date.day;
+
+
+        const dayNum = date.date;
+        const sunrise = astro.sunrise;
+        const sunset = astro.sunset;
+        const condition = d.condition.text;
+        const conditionCode = d.condition.code;
+        const maxtempC = d.maxtemp_c;
+        const mintempC = d.mintemp_c;
+        const maxtempF = d.maxtemp_f;
+        const mintempF = d.mintemp_f;
+        const totalprecipIn = d.totalprecip_in;
+        const totalprecipMm = d.totalprecip_mm;
+        const tsnowCm = d.totalsnow_cm;
+        const maxwindKph = d.maxwind_kph;
+        const maxwindMph = d.maxwindMph;
+         
+        const day = new Forecast(dayNum,sunrise,sunset,condition,conditionCode,maxtempC,mintempC,maxtempF,mintempF,totalprecipIn,totalprecipMm,tsnowCm,maxwindKph,maxwindMph);
+        
+        //Push object to array of 3 day forecast
+        forecastarray.push(day);
+
+    }
+
+    return forecastarray;
+}
+
+async function createObjects(array) {
+
+    const forecastInfo = await getData(array);   
+    return forecastInfo;
+}
+
+
 
 form.addEventListener('submit',submitForm)
 
