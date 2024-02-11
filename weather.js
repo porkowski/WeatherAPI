9eb62ade464e751aa3713fc18fdc96c9c5ca2a3d
porkowski <borkowskipatryk10@gmail.com>
2024-02-11 09:46:22 -0500

    working on saving data to object.

--- weather.js
+++ weather.js
@@ -1,6 +1,25 @@
+const { max } = require("date-fns");
+
 //Grab form elements from DOM
 const form = document.getElementById("formId");
 const text = document.getElementById("Location");
+
+//Create constructor for Forecast
+function Forecast(sunrise,sunset,condition,conditionCode,maxtempC,mintempC,maxtempF,mintempF,totalprecipIn,totalprecipMm,tsnowCm,maxwindKph,maxwindMph) {
+    this.sunrise=sunrise;
+    this.sunset=sunset;
+    this.condition=condition;
+    this.condition=conditionCode;
+    this.maxtempC=maxtempC;
+    this.mintempC=mintempC;
+    this.maxtempF=maxtempF;
+    this.mintempF=mintempF;
+    this.totalprecipIn=totalprecipIn;
+    this.totalprecipMm=totalprecipMm;
+    this.tsnowCm=tsnowCm;
+    this.maxwindKph=maxwindKph;
+    this.maxwindMph=maxwindMph;
+}
 
 async function getForecast(loc) {
     try {
@@ -8,7 +27,9 @@ async function getForecast(loc) {
         mode: 'cors'
     });
     const json = await response.json();
-    return json.forecast.forecastday;
+    const array = json.forecast.forecastday;
+    //Return array holding 3 days worth of weather for location
+    return array[0];
     } catch(error) {
         console.log(error);
     }
