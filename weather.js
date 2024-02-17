b67483d2179755bb7d04ba425e6fab1fb07f87db
porkowski <borkowskipatryk10@gmail.com>
2024-02-17 18:39:32 -0500

    succesfully displays weather in cards

--- weather.js
+++ weather.js
@@ -13,7 +13,7 @@ function Forecast(date,sunrise,sunset,condition,conditionCode,maxtempC,mintempC,
     this.sunrise=sunrise;
     this.sunset=sunset;
     this.condition=condition;
-    this.condition=conditionCode;
+    this.conditionCode=conditionCode;
     this.maxtempC=maxtempC;
     this.mintempC=mintempC;
     this.maxtempF=maxtempF;
@@ -62,9 +62,8 @@ function getData(array) {
         const tsnowCm = d.totalsnow_cm;
         const maxwindKph = d.maxwind_kph;
         const maxwindMph = d.maxwindMph;
-         
+
         const day = new Forecast(dayNum,sunrise,sunset,condition,conditionCode,maxtempC,mintempC,maxtempF,mintempF,totalprecipIn,totalprecipMm,tsnowCm,maxwindKph,maxwindMph);
-        
         //Push object to array of 3 day forecast
         forecastarray.push(day);
 
@@ -75,16 +74,11 @@ function getData(array) {
 
 
   
-async function createObjects(array) {
-
-    const forecastInfo = await getData(array);   
-    return forecastInfo;
-}
 
 async function storeForecast() {
     const location = text.value;
     const forecast = await getForecast(location);
-    const arrayWeather = await createObjects(forecast);
+    const arrayWeather = await getData(forecast);
     return arrayWeather;
 }
 
@@ -92,27 +86,26 @@ function displayInCard(forecastarray) {
     const img = document.querySelectorAll("img");
     img.forEach(image=> {
         const id = image.parentNode.parentNode.getAttribute('id');
-        let condition = forecastarray[id].condition;
+        let condition = forecastarray[id].conditionCode;
         if (condition == '1003') {
-            let code = 'pcloudy'
-        } else if (condition == '1006') {
-            let code ='cloudy'
+             code = 'pcloudy'
+        } else if (condition == '1006'|| condition == '1009') {
+              code ='Cloudy'
         } else if (condition == '1000') {
-            let code='sunny'
-        } else if (condition == '1030' || condition =='1063'|| condition =='1069' || condition == '1072' || condition=='1150'||condition=='1153'||condition=='1180'||condition=='1183'||condiiton='1198'||condition =='1204'||condition=='1240'||condition=='1239') {
-            let code='Lrain'
+              code='sunny'
+         } else if (condition == '1030' || condition =='1063'|| condition =='1069' || condition == '1072' || condition=='1150'||condition=='1153'||condition=='1180'||condition=='1183'||condition=='1198'||condition =='1204'||condition=='1240'||condition=='1239'|| condition =='1189') {
+             code='Lrain'
         }  else if (condition == '1066'|| condition == '1114'|| condition =='1117'||condition=='1210'||condition=='1213'||condition=='1216'||condition=='1219'||condition=='1222'||condition=='1225'||condition=='1255'||condition=='1258') {
-            let code='snow'
+             code='Snow'
         } else if (condition == '1087'||condition=='1273'||condition=='1276'||condition=='1279'||condition=='1282') {
-            let code='Tshower'
+             code='Tshower'
         } else if (condition == '1135' || condition == '1147') {
-            let code ='Foggy'
+             code ='Foggy'
         } else if (condition =='1171'||condition=='1186'||condition=='1192'||condition=='1195'|| condition=='1201'||condition=='1207'||condition=='1243'||condition=='1246'||condition=='1252') {
-            let code='Rain'
+             code='Rain'
         } else if (condition=='1237'||condition=='1261'||condition=='1264') {
-            let code='Hail'
+             code='Hail'
         }
-
 
         image.setAttribute('src','weather/'+code+'.png');
     })
